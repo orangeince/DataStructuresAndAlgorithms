7b87348a7b5dad0d7c9db35a7e8688f8e244efb7
Shao <orangeince@outlook.com>
2019-03-29 19:22:00 +0800

    add: LinkedList for queue.

--- DataStructuresAndAlgorithms/Queue.playground/Contents.swift
+++ DataStructuresAndAlgorithms/Queue.playground/Contents.swift
@@ -1,7 +1,14 @@
 import Foundation
 
+protocol QueueType {
+    associatedtype T
+    func enqueue(_ element: T) -> Bool
+    func dequeue() -> T?
+    func printDetail()
+}
+
 /// Implement queue using array
-class ArrayQueue<T> {
+class ArrayQueue<T>: QueueType {
     private var elements: [T] = []
     private(set) var capacity: Int = 0
     private var headIdx: Int = 0
@@ -51,7 +58,57 @@ class Node<T> {
     }
 }
 
-let queue = ArrayQueue<Int>(capacity: 3)
+class LinkedListQueue<T>: QueueType {
+    private(set) var capacity: Int
+    private var count: Int = 0
+    private var head: Node<T>?
+    private var tail: Node<T>?
+    
+    var isEmpty: Bool {
+        return head == nil
+    }
+    
+    var isFull: Bool {
+        return count == capacity
+    }
+
+    init(capacity: Int) {
+        self.capacity = capacity
+    }
+    
+    func enqueue(_ element: T) -> Bool {
+        guard !isFull else { return false }
+        let node = Node(element)
+        if head == nil {
+            head = node
+        }
+        tail?.next = node
+        tail = node
+        return true
+    }
+    
+    func dequeue() -> T? {
+        guard let element = head?.element else { return nil }
+        head = head?.next
+        if head == nil {
+            tail = nil
+        }
+        return element
+    }
+    
+    func printDetail() {
+        print("head: \(head?.element)", "tail: \(tail?.element)", separator: ", ", terminator: " | ")
+        var node = head
+        while let n = node {
+            print(n.element, terminator: " ")
+            node = node?.next
+        }
+        print()
+    }
+}
+
+//let queue: QueueType = ArrayQueue<Int>(capacity: 3)
+let queue = LinkedListQueue<Int>(capacity: 3)
 queue.enqueue(1)
 queue.enqueue(2)
 queue.enqueue(3)
