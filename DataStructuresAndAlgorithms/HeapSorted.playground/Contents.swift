cf01cc5d082c9b6cab2a7849860945dd258425a9
Shao <orangeince@outlook.com>
2020-09-13 15:34:18 +0800

    refactor: 优化堆的实现代码，并增加“大顶堆”和“小顶堆”的类型控制

--- DataStructuresAndAlgorithms/HeapSorted.playground/Contents.swift
+++ DataStructuresAndAlgorithms/HeapSorted.playground/Contents.swift
@@ -1,104 +1,38 @@
 import Foundation
 
 class Heap {
-    private var values: [Int] = [0]
-
-    init(values: [Int] = []) {
-        self.values = self.values + values
-        var i = values.count / 2
-        while i >= 1 {
-            heapify(at: i, endIndex: values.count - 1)
-            i -= 1
+    enum HeapType: Hashable {
+        case max
+        case min
+    }
+    var values: [Int]
+    var preferredComparison: (Int, Int) -> Bool
+    let type: HeapType
+    var valueCount: Int {
+        return values.count - 1
+    }
+    init(values: [Int] = [], type: HeapType = .max) {
+        self.type = type
+        switch type {
+        case .max:
+            preferredComparison = { $0 > $1}
+        case .min:
+            preferredComparison = { $0 < $1 }
+        }
+        self.values = [0] + values
+        for i in (1 ... valueCount / 2 ).reversed() {
+            heapify(beginAt: i, endAt: valueCount)
         }
     }
     
-    func sorted() -> [Int] {
-        var cache = values
-        var k = values.count - 1
-        while k > 1 {
-            swap(1, k)
-            k -= 1
-            heapify(at: 1, endIndex: k)
-        }
-        (cache, values) = (values, cache)
-        cache.removeFirst()
-        return cache
-    }
-    
-    func insert(value: Int) {
-        let index = values.count
-        values.append(value)
-        adjust(at: index)
-    }
-    
-    func find(value: Int) -> Int? {
-        guard values.count > 1 else {
-            return nil
-        }
-        
-        func find(value: Int, startAt: Int) -> Int? {
-            guard startAt < values.count else {
-                return nil
-            }
-            if values[startAt] == value {
-                return startAt
-            } else if values[startAt] < value {
-                return nil
-            } else {
-                if let idx = find(value: value, startAt: 2*startAt) {
-                    return idx
-                } else {
-                    return find(value: value, startAt: 2*startAt + 1)
-                }
-            }
-        }
-        
-        return find(value: value, startAt: 1)
-    }
-    
-    func delete(value: Int) {
-        guard let idx = find(value: value) else {
-            return
-        }
-        let end = values.count - 1
-        (values[end], values[idx]) = (values[idx], values[end])
-        values.removeLast()
-        lowAdjust(at: idx)
-    }
-    
-    private func lowAdjust(at index: Int) {
-        guard index < values.count && index*2 < values.count else {
-            return
-        }
-        let left = index * 2
-        let right = index * 2 + 1
-        if right >= values.count {
-            if values[left] > values[index] {
-                swap(left, index)
-            }
-            return
-        }
-        if values[right] > values[left] {
-            if values[right] > values[index] {
-                swap(right, index)
-            }
-            lowAdjust(at: right)
-        } else {
-            if values[left] > values[index] {
-                swap(left, index)
-            }
-            lowAdjust(at: left)
-        }
-
-    }
-    private func heapify(at index: Int, endIndex: Int) {
+    private func heapify(beginAt index: Int, endAt: Int) {
         var i = index
         while true {
             var maxPos = i
-            if i * 2 <= endIndex && values[i] < values[i * 2] {
+            if i * 2 <= endAt && preferredComparison(values[i * 2], values[maxPos]) {
                 maxPos = i * 2
             }
-            if i * 2 + 1 <= endIndex && values[maxPos] < values[i * 2 + 1] {
+            if i * 2 + 1 <= endAt && preferredComparison(values[i * 2 + 1], values[maxPos]) {
                 maxPos = i * 2 + 1
             }
             if maxPos == i {
@@ -108,24 +42,67 @@ class Heap {
             i = maxPos
         }
     }
-
-    private func swap(_ a: Int, _ b: Int) {
-        (values[a], values[b]) = (values[b], values[a])
+    
+    private func swap(_ i: Int, _ j: Int) {
+        (values[i], values[j]) = (values[j], values[i])
     }
     
-    private func adjust(at index: Int) {
-        guard index > 1 else {
-            return
+    private func findIndex(of value: Int, startAt index: Int) -> Int? {
+        guard index < values.count else {
+            return nil
         }
-        let p = index / 2
-        guard values[p] < values[index] else {
-            return
+        if values[index] == value {
+            return index
+        } else if !preferredComparison(values[index], value) {
+            return nil
+        } else {
+            if let idx = findIndex(of: value, startAt: index * 2) {
+                return idx
+            } else {
+                return findIndex(of: value, startAt: index * 2 + 1)
+            }
         }
-        (values[p], values[index]) = (values[index], values[p])
-        adjust(at: p)
     }
-
-    func showAll() {
+    
+    func insert(_ value: Int) {
+        values.append(value)
+        var i = values.count - 1
+        while i >= 1 {
+            let p = i / 2
+            guard preferredComparison(values[i], values[p]) else {
+                return
+            }
+            swap(i, p)
+            i = p
+        }
+    }
+    
+    func delete(_ value: Int) {
+        guard let idx = findIndex(of: value, startAt: 1)
+            else { return }
+        swap(idx, values.count - 1)
+        values.removeLast()
+        heapify(beginAt: idx, endAt: values.count - 1)
+    }
+    
+    func find(_ value: Int) -> Bool {
+        return findIndex(of: value, startAt: 1) != nil
+    }
+    
+    func sorted() -> [Int] {
+        var cache = values
+        var i = valueCount
+        while i > 1 {
+            swap(1, i)
+            i -= 1
+            heapify(beginAt: 1, endAt: i)
+        }
+        (cache, values) = (values, cache)
+        cache.removeFirst()
+        return type == .min ? cache.reversed() : cache
+    }
+    
+    func printAll() {
         guard values.count > 1 else {
             print("heap is empty")
             return
@@ -150,22 +127,7 @@ class Heap {
         }
         print()
     }
-
 }
-//let h = Heap(values: [0,7,5,6,4,2,1])
-//h.insert(value: 3)
-//h.insert(value: 8)
-//h.insert(value: 9)
-//h.insert(value: 19)
-//h.insert(value: 11)
-//h.insert(value: 12)
-//h.insert(value: 13)
-//h.insert(value: 14)
-//h.insert(value: 7)
-//h.insert(value: 27)
-//h.showAll()
-//h.delete(value: 6)
-//h.showAll()
 let h = Heap(values: [7,5,19,8,4,1,20,13,16])
-h.showAll()
+h.printAll()
 h.sorted()
